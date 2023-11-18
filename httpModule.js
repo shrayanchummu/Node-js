0607bcd170501c1fac518e6b7c3fab0ef0258b13
shrayanchummu <shrayanchummu@gmail.com>
2023-11-19 01:45:58 +0530

    Basic HTTP module with Req and Res version

--- httpModule.js
+++ httpModule.js
@@ -1,8 +1,13 @@
 const http=require('http');
-const server=http.createServer();
-
-server.on('connection',(socket)=>{
-    console.log('New Connection');
+const server=http.createServer((req,res)=>{
+    if(req.url==='/'){
+        res.write('You are in Home Page');
+        res.end();
+    }
+    if(req.url==='/api'){
+        res.write('You are in /api page');
+        res.end();
+    }
 });
 
 server.listen(3000);
