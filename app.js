853fcf736719cdf48a0f076330ddafdb703b25e9
shrayanchummu <shrayanchummu@gmail.com>
2023-11-18 12:17:50 +0530

    modifications in logger and app.js for loading module

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var logger = require('./logger')
+const log = require('./logger')
 // require('./name.js') #to indicate current folder directory
 // require('./subFolder/name.js') #to indicate subFolder folder directory
 // require('../name.js') #to indicate parent folder directory
 
-logger.log("Hello World");
+log("Hello World");
--- logger.js
+++ logger.js
@@ -5,5 +5,7 @@ function log(message){
 }
 
 // module.exports.publicInterface=privateInterface;
-module.exports.endPoint=url;
-module.exports.log=log;
+// module.exports.endPoint=url;
+// module.exports.log=log;
+
+module.exports=log; // to export function only instead of object "log"
