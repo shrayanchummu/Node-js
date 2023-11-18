9585b09c067bb4ec381f1798e7bbdcefda704b71
shrayanchummu <shrayanchummu@gmail.com>
2023-11-18 13:02:46 +0530

    Event module modifications

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const EventEmitter=require('events'); // EventEmitter is a class
 const emitter = new EventEmitter(); // creating a object
 
 // Register a Listener
-emitter.on('messageLogged', function(){
-    console.log('Listener called');
+emitter.on('messageLogged', function(event){
+    console.log('Listener called',event);
 });
 //Raise an Event
-emitter.emit('messageLogged');
+emitter.emit('messageLogged',{ id:1, url:'https://'});
