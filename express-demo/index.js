8cdd7b870faee5e1e6fd040296ed2f96722c9849
shrayanchummu <shrayanchummu@gmail.com>
2023-12-14 01:02:34 +0530

    PUT REQUEST IN EXPRESS.JS

--- express-demo/index.js
+++ express-demo/index.js
@@ -11,13 +11,45 @@ const names = [
     {id:3,name:'NAME3'},
 ];
 
+app.put('/api/names/:id',(req, res) => {
+    // Look up the names
+    // If not found throw 404 error
+    const name=names.find(n => n.id === parseInt(req.params.id));
+    if(!name)
+    {
+        res.status(404).send('given id not found');
+    }
+    // If found,Validate the request
+    const schema={
+        name:Joi.string().min(3).required()
+    };
+    const result=Joi.validate(req.body,schema);
+    // If invalid,throw an 400 Bad Request error
+    if(result.error)
+    {
+        // 400 Bad Request
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+    // If valid, Update the names
+    // Return the updated names
+    names.name=req.body.name;
+    res.send(names);
+
+});
+
+
+
+
+
+
+
 app.post('/api/names',(req, res)=>{
 
     const schema={
         name:Joi.string().min(3).required()
     };
     const result=Joi.validate(req.body,schema);
-    console.log(result);
 
     if(result.error)
     {
