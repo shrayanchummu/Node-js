66f2a41290e8a786ebcd91d32349bf250ba14e66
shrayanchummu <shrayanchummu@gmail.com>
2023-12-13 23:02:58 +0530

    EXPRESS.JS => CRUD operations & Nodemon

---
+++ express-demo/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World!, You are on Port 3000');
+});
+app.get('/api', (req, res)=>{
+    res.send([1,2,3]);
+});
+
+
+const port = process.env.PORT || 3000
+app.listen(port,()=>{
+    console.log(`Listening on port ${port}`);
+});
+// this app object have all these methods
+// app.get(); ('path',callback function)
+// app.post();
+// app.put();
+// app.delete();
