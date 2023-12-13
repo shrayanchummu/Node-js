adfc1a4fcc99c9f5f1d119b9e462417b31998265
shrayanchummu <shrayanchummu@gmail.com>
2023-12-14 01:54:18 +0530

    keeping the order of get,post,put, delete in the index.js file

--- express-demo/index.js
+++ express-demo/index.js
@@ -2,8 +2,13 @@ const Joi= require('joi')
 const express = require('express');
 const app = express();
 
-app.use(express.json()); 
+// this app object have all these methods
+// app.get(); ('path',callback function)
+// app.post();
+// app.put();
+// app.delete();
 
+app.use(express.json()); 
 
 const names = [
     {id:1,name:'NAME1'},
@@ -11,96 +16,12 @@ const names = [
     {id:3,name:'NAME3'},
 ];
 
-app.delete('/api/names/:id',(req, res) =>{
-    // Look up the names
-    // if not found,return 404
-    const name=names.find(n => n.id === parseInt(req.params.id));
-    if(!name)
-    {
-        return res.status(404).send('given id not found');
-    }
-    // if found,delete the name and return it.
-    const index= names.indexOf(name);
-    names.splice(index,1);
-    res.send(name);
-});
-
-app.put('/api/names/:id',(req, res) => {
-    const name=names.find(n => n.id === parseInt(req.params.id));
-    if(!name)
-    {
-        return res.status(404).send('given id not found');
-    }
-
-    const {error} = validateNames(req.body);
-    if(error)
-    {
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
-
-
-    // If valid, Update the names
-    // Return the updated names
-    name.name=req.body.name;
-    res.send(names);
-
-});
-
-//function to validate names
-function validateNames(name)
-{
-    const schema={
-        name:Joi.string().min(3).required()
-    };
-    return Joi.validate(name,schema);
-}
-
-
-
-
-
-
-
-app.post('/api/names',(req, res)=>{
-
-    // const schema={
-    //     name:Joi.string().min(3).required()
-    // };
-    // const result=Joi.validate(req.body,schema);
-
-    // if(result.error)
-    // {
-    //     // 400 Bad Request
-    //     res.status(400).send(result.error.details[0].message);
-    //     return;
-    // }
-
-    const {error} = validateNames(req.body);
-    if(error)
-    {
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
-
-
-    const newName={
-        id:names.length+1 ,
-        name:req.body.name
-    };
-    names.push(newName);
-    res.send(names);
-});
-
-
-
-
-
-
-
-
-
-
+// app.get('/api/:id/:name', (req, res)=>{
+//     res.send(req.params);
+// });
+// app.get('/api/:id/:name', (req, res)=>{
+//     res.send(req.query);
+// });
 
 app.get('/', (req, res) => {
     res.send('Hello World!, You are on Port 3000');
@@ -117,29 +38,82 @@ app.get('/api/names/:id', (req, res)=>{
     else
     {
         res.send(name);
-
     }
 });
 
 
-// app.get('/api/:id/:name', (req, res)=>{
-//     res.send(req.params);
-// });
-// app.get('/api/:id/:name', (req, res)=>{
-//     res.send(req.query);
-// });
+//function to validate names
+function validateNames(name)
+{
+    const schema={
+        name:Joi.string().min(3).required()
+    };
+    return Joi.validate(name,schema);
+}
 
+app.post('/api/names',(req, res)=>{
+
+    // const schema={
+    //     name:Joi.string().min(3).required()
+    // };
+    // const result=Joi.validate(req.body,schema);
+
+    // if(result.error)
+    // {
+    //     // 400 Bad Request
+    //     res.status(400).send(result.error.details[0].message);
+    //     return;
+    // }
+    const {error} = validateNames(req.body);
+    if(error)
+    {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+    const newName={
+        id:names.length+1 ,
+        name:req.body.name
+    };
+    names.push(newName);
+    res.send(names);
+});
+
+
+
+app.put('/api/names/:id',(req, res) => {
+    const name=names.find(n => n.id === parseInt(req.params.id));
+    if(!name)
+    {
+        return res.status(404).send('given id not found');
+    }
+    const {error} = validateNames(req.body);
+    if(error)
+    {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+    // If valid, Update the names
+    // Return the updated names
+    name.name=req.body.name;
+    res.send(names);
+
+});
+
+app.delete('/api/names/:id',(req, res) =>{
+    // Look up the names
+    // if not found,return 404
+    const name=names.find(n => n.id === parseInt(req.params.id));
+    if(!name)
+    {
+        return res.status(404).send('given id not found');
+    }
+    // if found,delete the name and return it.
+    const index= names.indexOf(name);
+    names.splice(index,1);
+    res.send(name);
+});
 
 const port = process.env.PORT || 3000
 app.listen(port,()=>{
     console.log(`Listening on port ${port}`);
 });
-
-
-
-
-// this app object have all these methods
-// app.get(); ('path',callback function)
-// app.post();
-// app.put();
-// app.delete();
