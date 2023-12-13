516502d9fd2dc895ec6a2f0aa2e07074818d94c2
shrayanchummu <shrayanchummu@gmail.com>
2023-12-13 23:28:26 +0530

    GET request with names=[]

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,15 +1,34 @@
 const express = require('express')
 const app = express()
 
+const names = [
+    {id:1,name:'NAME1'},
+    {id:2,name:'NAME2'},
+    {id:3,name:'NAME3'},
+];
 app.get('/', (req, res) => {
     res.send('Hello World!, You are on Port 3000');
 });
-app.get('/api', (req, res)=>{
-    res.send([1,2,3]);
+app.get('/api/names', (req, res)=>{
+    res.send(names);
 });
-app.get('/api/:id/:name', (req, res)=>{
-    res.send(req.params);
+app.get('/api/names/:id', (req, res)=>{
+    const name=names.find(n => n.id === parseInt(req.params.id));
+    if(!name)
+    {
+        res.status(404).send('given id not found');
+    }
+    else
+    {
+        res.send(name);
+
+    }
 });
+
+
+// app.get('/api/:id/:name', (req, res)=>{
+//     res.send(req.params);
+// });
 // app.get('/api/:id/:name', (req, res)=>{
 //     res.send(req.query);
 // });
