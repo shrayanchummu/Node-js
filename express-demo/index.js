b31fa66de887432b0fb669e569672dd0842fa655
shrayanchummu <shrayanchummu@gmail.com>
2023-12-13 23:17:25 +0530

    Routing parameters and Query parameters

--- express-demo/index.js
+++ express-demo/index.js
@@ -7,12 +7,22 @@ app.get('/', (req, res) => {
 app.get('/api', (req, res)=>{
     res.send([1,2,3]);
 });
+app.get('/api/:id/:name', (req, res)=>{
+    res.send(req.params);
+});
+// app.get('/api/:id/:name', (req, res)=>{
+//     res.send(req.query);
+// });
 
 
 const port = process.env.PORT || 3000
 app.listen(port,()=>{
     console.log(`Listening on port ${port}`);
 });
+
+
+
+
 // this app object have all these methods
 // app.get(); ('path',callback function)
 // app.post();
