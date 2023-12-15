d9ee51b9a2ae9351358968032375206a3431a008
shrayanchummu <shrayanchummu@gmail.com>
2023-12-16 00:20:07 +0530

    converting callbacks into promises

--- async-demo/promise.js
+++ async-demo/promise.js
@@ -1,10 +1,39 @@
-const p = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        console.log('blah blah blah');
-        //resolve('Answer');
-        reject(new Error('Error'));
-    }, 2000);
-});
+// const p = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log('blah blah blah');
+//         resolve('Answer'); // pending -> fulfilled
+//         reject(new Error('Error')); // pending -> rejected
+//     }, 2000);
+// });
 
-p.then((result) => {console.log(result);})
-.catch((err) => {console.log(err.message)});
+// p.then((result) => {console.log(result);})
+// .catch((err) => {console.log(err.message)});
+
+function getUser(id)
+{
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            console.log('getting username'); // executes in future after 2000ms
+            // callback is called when the result of the asynchronous operation is ready
+            resolve({id: id,username:'Chummu'});
+        },2000);
+    }); 
+}
+function getRepositories(username)
+{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log('getting repositories of '+username +' by gitbuh API'); // executes in future after 2000ms
+            resolve(['repo1','repo2','repo3']);
+        },2000);
+    });
+}
+
+const p= getUser(1);
+p.then((user)=>{
+    console.log(user);
+    return getRepositories(user.username);
+})
+.then((repo)=>{
+    console.log(repo);
+})
