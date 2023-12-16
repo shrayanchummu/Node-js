0852ff415061e08699ee16f81a3c426bfff0244a
shrayanchummu <shrayanchummu@gmail.com>
2023-12-16 23:56:28 +0530

    Complex Queries : Logical Operators

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -43,6 +43,9 @@ async function getModel(){
         // .find({price: {$gt: 10, $lt: 20}}) // greater than 10 & less than 20
         // .find({price: {$in:[10,20,30]}}) // if price = 10 or 20 or 30
         // .find({price: {$ne:10}}) // if price not equal to 10
+        .find()
+        .or([{name:'Shrayan'},{isPublished:true}]) // gets Shrayan or true values
+        .and([{name:'Shrayan'},{isPublished:false}])// gets only if 
         .limit(10)
         .sort({name :1}) // 1/-1 => Ascending/Descending
         .select({ name:1,tags:1}) // shows only name,tag info
