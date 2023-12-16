3c2c3b82921b4d858af38f3b3cddc9ac61c837eb
shrayanchummu <shrayanchummu@gmail.com>
2023-12-16 23:05:44 +0530

    connected to MONGODB

---
+++ mongo-demo/index.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/database_name');
+// if db is not created already, it will create it
+// if its production environment, link should come from configuration file
+mongoose.connect('mongodb://127.0.0.1/playground')
+    .then(()=>{console.log('Connected to MongoDB');})
+    .catch(err=>{console.error('Error connecting to MongoDB ',err.message);});
