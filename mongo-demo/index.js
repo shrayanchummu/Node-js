4bbf8d8ce662b8ccb6244eb9ffe46828d51e9330
shrayanchummu <shrayanchummu@gmail.com>
2023-12-16 23:24:31 +0530

    creating model from schema

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -7,10 +7,23 @@ mongoose.connect('mongodb://127.0.0.1/playground')
     .catch(err=>{console.error('Error connecting to MongoDB ',err.message);});
 
 // create a schema
-
 const userSchema = new mongoose.Schema({
     name: String,
     tags:[String],
     date:{ type: Date, default: Date.now },
     isPublished: Boolean
 });
+
+// Classes, Objects in OOPS
+// Schema, Model in Mongoose
+async function createModel(){
+    const User = mongoose.model('User', userSchema);
+    const user = new User({
+    name:'Shrayan',
+    tags:['JavaScript','Node.js'],
+    isPublished:true
+    });
+    const result = await user.save();
+    console.log(result);
+}
+createModel();
