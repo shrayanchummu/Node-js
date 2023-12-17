3d51b45d5177ab70c339fb819af531cdf10c01d4
shrayanchummu <shrayanchummu@gmail.com>
2023-12-17 20:37:28 +0530

    AFTER : Mongoose API

--- mongo-demo/api-index.js
+++ mongo-demo/api-index.js
@@ -1,8 +1,17 @@
 // order of the elements [require,set,use]
-const Joi= require('joi');
 const express = require('express');
 const api_names=require('./api-names');
 const app = express();
+
+// Mongoose
+const mongoose= require('mongoose');
+mongoose.connect('mongodb://127.0.0.1/api-names')
+  .then(()=>{
+        console.log('connected to mongodb');
+    })
+  .catch(err=>{
+       console.log(err.message);
+   });
 
 app.use(express.json());
 app.use('/api/names',api_names);
--- mongo-demo/api-names.js
+++ mongo-demo/api-names.js
@@ -1,18 +1,37 @@
+const Joi= require('joi');
 const express = require('express');
 const router = express.Router();
 
-const names = [
-    {id:1,name:'NAME1'},
-    {id:2,name:'NAME2'},
-    {id:3,name:'NAME3'},
-];
+// Mongoose
+const mongoose= require('mongoose');
 
-router.get('/', (req, res)=>{
+const namesSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        minLength:5,
+        maxLength:20
+    },
+    // id is inbuilt
+});
+
+const Name= mongoose.model('Name',namesSchema);
+
+
+// const names = [
+//     {id:1,name:'NAME1'},
+//     {id:2,name:'NAME2'},
+//     {id:3,name:'NAME3'},
+// ];
+
+router.get('/', async(req, res)=>{
+    const names= await Name.find().sort('name'); 
     res.send(names);
 });
 
-router.get('/:id', (req, res)=>{
-    const name=names.find(n => n.id === parseInt(req.params.id));
+router.get('/:id', async(req, res)=>{
+    const name =await Name.findById(req.params.id)
+    // const name=names.find(n => n.id === parseInt(req.params.id));
     if(!name)
     {
         return res.status(404).send('given id not found');
@@ -33,46 +52,49 @@ function validateNames(name)
     return Joi.validate(name,schema);
 }
 
-router.post('/',(req, res)=>{
+router.post('/',async(req, res)=>{
+
+    const {error} = validateNames(req.body);
+    if(error)
+    {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    let newName=new Name({ name:req.body.name });
+    names=await newName.save();
+    res.send(names);
+});
+
+router.put('/:id',async(req, res) => {
     const {error} = validateNames(req.body);
     if(error)
     {
         res.status(400).send(result.error.details[0].message);
         return;
     }
-    const newName={
-        id:names.length+1 ,
-        name:req.body.name
-    };
-    names.push(newName);
-    res.send(names);
-});
-
-router.put('/:id',(req, res) => {
-    const name=names.find(n => n.id === parseInt(req.params.id));
+    const name=await Name.findByIdAndUpdate(req.params.id,{
+        name:req.body.name,
+        new:true
+    })
+    // const name=names.find(n => n.id === parseInt(req.params.id));
     if(!name)
     {
         return res.status(404).send('given id not found');
     }
-    const {error} = validateNames(req.body);
-    if(error)
-    {
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
-    name.name=req.body.name;
+    // name.name=req.body.name;
     res.send(names);
-
 });
 
-router.delete('/:id',(req, res) =>{
-    const name=names.find(n => n.id === parseInt(req.params.id));
+router.delete('/:id',async(req, res) =>{
+    const name = await Name.findByIdAndDelete(req.params.id)
+    // const name=names.find(n => n.id === parseInt(req.params.id));
     if(!name)
     {
         return res.status(404).send('given id not found');
     }
-    const index= names.indexOf(name);
-    names.splice(index,1);
+    // const index= names.indexOf(name);
+    // names.splice(index,1);
     res.send(name);
 });
 
