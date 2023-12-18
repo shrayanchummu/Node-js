9c674c3bcd2fdf18fa44ccd126dac764c393137a
shrayanchummu <shrayanchummu@gmail.com>
2023-12-19 00:43:43 +0530

    REFERENCES: Using Embedding

--- mongo-demo/embedding.js
+++ mongo-demo/embedding.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/playground')
+mongoose.connect('mongodb://127.0.0.1/playground')
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
@@ -13,7 +13,8 @@ const authorSchema = new mongoose.Schema({
 const Author = mongoose.model('Author', authorSchema);
 
 const Course = mongoose.model('Course', new mongoose.Schema({
-  name: String
+  name: String,
+  author: authorSchema
 }));
 
 async function createCourse(name, author) {
@@ -31,4 +32,4 @@ async function listCourses() {
   console.log(courses);
 }
 
-createCourse('Node Course', new Author({ name: 'Mosh' }));
+createCourse('How to Flirt', new Author({ name: 'Chummu' }));
